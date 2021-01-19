c45585de377df86a5e78fc307de8e7c27824d477
AJStieren <Arthur.Stieren@Rackspace.com>
2021-01-19 16:22:25 -0600

    Sanitized terrform start

---
+++ main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+    token = ""
+}
+
+resource "cloudfeeds_instance" "terraform-web" {
+    image = ""
+    label = ""
+    group = ""
+    region = ""
+    type = ""
+    authorized_keys = [ "" ]
+    root_pass = ""
+}
