88aaadaf204d9f1a35c757852bdecfa7fa7c45fb
kavishkhanna <kavish.khanna@rackspace.com>
2022-09-01 17:58:06 +0530

    Commenting the variables for Data Dog

--- terraform/aws/iam/variables.tf
+++ terraform/aws/iam/variables.tf
@@ -7,6 +7,14 @@ variable "datadog_aws_integration_external_id" {
   description = ""
 }
 
+#variable "aws_account_id" {
+#  description = "AWS account id for datadog integration configuration"
+#}
+
+#variable "environment" {
+#  description = "The current active environment"
+#}
+
 variable "table_arn" {
   description = "Arn of the cloudfeeds table resource for IAM policy control"
 }
